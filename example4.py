2224bb7132ce68aabe922af2cfb7e44c8fa0ac3a
0rxan <46538661+0rxan@users.noreply.github.com>
2020-02-23 22:45:55 +0400

    commit dev1

--- example4.py
+++ example4.py
@@ -6,9 +6,10 @@ print(sum(5,6))
 
 while True:
     number1 = input("Number1")
+    number2 = input("Number1")
     print(number1)
     print(sum(5,6))
     print(sum(5,6))
-    print(sum(5,6))
-    print(sum(5,6))
+    print(sum(8,6))
+    print(sum(5,9))
     print(sum(5,6))
